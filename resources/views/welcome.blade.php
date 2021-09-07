e4d2037ee6101d1e28297a4f484c303f7c7117ea
Theanhpham11520 <panh23368@gmail.com>
2021-09-07 22:22:26 +0700

    [DTN-11] Design database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    do an tot nghiep
+                    Create Database
                 </div>
 
                 <div class="links">
