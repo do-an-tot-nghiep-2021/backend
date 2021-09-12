95ac36eb52b50c066d1ce7dafc102d86aee84aef
chungnq253 <chungnq253@gmail.com>
2021-09-12 22:15:19 +0700

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Test trigger deploy
+                   Test trigger deploy pull request
                 </div>
 
                 <div class="links">
