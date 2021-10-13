6b310775beb2c596f4e640a854f8f10e38e92e33
Theanhpham11520 <panh23368@gmail.com>
2021-10-13 21:32:06 +0700

    [update_api]type_product, topping_product

--- app/Models/ProductsModel.php
+++ app/Models/ProductsModel.php
@@ -10,4 +10,12 @@ class ProductsModel extends Model
     protected $fillable = [
         'name', 'image', 'price', 'description', 'point', 'cate_id'
     ];
+
+    public function productTopping(){
+        return $this->belongsToMany(ToppingModel::class,'product_topping','product_id', 'topping_id');
+    }
+
+    public function productType(){
+        return $this->belongsToMany(TypeModel::class,'product_types','product_id', 'type_id');
+    }
 }
